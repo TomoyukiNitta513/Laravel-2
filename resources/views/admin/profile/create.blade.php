732a290c959d398325724155f2070afebabe017f
tomoyuki <foucchi2jv@icloud.com>
2019-06-19 13:43:49 +0000

    PHP/Laravel 13

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,27 +1,48 @@
-<!--<!DOCTYPE html>-->
-<!--<html>-->
-<!--    <head>-->
-<!--        <meta charset="utf-8">-->
-<!--        <meta http-equiv="X-UA-Compatible" content="IE=edge">-->
-<!--        <meta name="viewport" content="width=device-width, initial-scale=1">-->
-        
-<!--        <title>MyProfile</title>-->
-<!--    </head>-->
-<!--    <body>-->
-<!--        <h1>Myプロフィール作成画面</h1>-->
-<!--    </body>-->
-<!--</html>-->
 @extends('layouts.profile')
-
-@section('title', 'My プロフィール')
+@section('title', 'プロフィールの新規作成')
 
 @section('content')
     <div class="container">
-        <div class="row">
-            <div class="col-md-8 mx-auto">
-                <h2>My プロフィール</h2>
-            </div>
-        </div>
-        
+       <div class="row">
+          <div class="col-md-8 mx-auto">
+             <h2>プロフィールの新規作成</h2> 
+             <form action="{{ action('Admin\ProfileController@create') }}" method="post" enctype="multipart/form-data">
+                 
+                 @if (count($errors) > 0)
+                 <ul>
+                     @foreach($errors->all() as $e)
+                         <li>{{ $e }}</li>
+                     @endforeach
+                 </ul>
+                 @endif
+                 <div class="form-group row">
+                     <label class="col-md-2" for="name">氏名</label>
+                     <div class="col-md-10">
+                         <input type="text" class="form-control" name="name" value="{{ old('name') }}"></input>
+                     </div>
+                 </div>
+                 <div class="form-group row">
+                     <label class="col-md-2" for="gender">性別</label>
+                     <div class="col-md-10">
+                         <input type="text" class="form-control" name="gender" value="{{ old('gender') }}"></input>
+                     </div>
+                 </div>
+                 <div class="form-group row">
+                     <label class="col-md-2" for="introduction">自己紹介欄</label>
+                     <div class="col-md-10">
+                         <textarea class="form-control" name="introduction" rows="10">{{ old('introduction') }}</textarea>
+                     </div>
+                 </div>
+                 <div class="form-group row">
+                     <label class="col-md-2" for="hobby">趣味</label>
+                     <div class="col-md-10">
+                         <textarea class="form-control" name="hobby" rows="5">{{ old('hobby') }}</textarea>
+                     </div>
+                 </div>
+                 {{ csrf_field() }}
+                 <input type="submit" class="btn btn-primary" value="更新">
+             </form>
+          </div> 
+       </div> 
     </div>
 @endsection
