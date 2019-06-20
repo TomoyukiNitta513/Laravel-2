c018e26c3820d18138f5509dc6157721e20fbaf8
tomoyuki <foucchi2jv@icloud.com>
2019-06-20 12:05:49 +0000

    PHP/Laravel 14

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Profile;
 
 class ProfileController extends Controller
 {
@@ -13,7 +14,17 @@ class ProfileController extends Controller
         
     }
     
-    public function create() {
+    public function create(Request $request) {
+        $this->validate($request, Profile::$rules);
+        
+        $profiles = new Profile;
+        $form = $request->all();
+        
+        unset($form['_token']);
+        
+        $profiles->fill($form);
+        $profiles->save();
+        
         return redirect('admin/profile/create');
         
     }
