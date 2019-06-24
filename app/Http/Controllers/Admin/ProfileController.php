dec0ea76ad4965a9202616c7b6dafe3fd2c348d6
tomoyuki <foucchi2jv@icloud.com>
2019-06-24 10:28:48 +0000

    PHP/Laravel 16

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -8,6 +8,16 @@ use App\Profile;
 
 class ProfileController extends Controller
 {
+    public function index(Request $request) {
+        $cond_name = $request->cond_name;
+        if($cond_name != '') {
+            $posts = Profile::where('name', $cond_name)->get();
+        } else {
+            $posts = Profile::all();
+        }
+        return view('admin.profile.index', ['posts' => $posts, 'cond_name' => $cond_name]);
+    }
+    
     //
     public function add() {
         return view('admin.profile.create');
@@ -17,26 +27,37 @@ class ProfileController extends Controller
     public function create(Request $request) {
         $this->validate($request, Profile::$rules);
         
-        $profiles = new Profile;
+        $profile = new Profile;
         $form = $request->all();
         
         unset($form['_token']);
         
-        $profiles->fill($form);
-        $profiles->save();
+        $profile->fill($form);
+        $profile->save();
         
         return redirect('admin/profile/create');
         
     }
     
-    public function edit() {
-        return view('admin.profile.edit');
-        
+    public function edit(Request $request){
+        $profile = Profile::find($request->id);
+        if (empty($profile)) {
+            abort(404);
+        }
+        return view('admin.profile.edit', ['profile_form' => $profile]);
     }
     
-    public function update() {
-        return redirect('admin/profile/edit');
+    public function update(Request $request) {
+        $this->validate($request, Profile::$rules);
         
+        $profile = Profile::find($request->id);
+        $profile_form = $request->all();
+        
+        unset($profile_form['_token']);
+        
+        $profile->fill($profile_form)->save();
+        
+        return redirect('admin/profile/edit');
     }
 }
 
